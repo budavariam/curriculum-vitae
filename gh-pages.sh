59a906e9c209124eb25485b0ee6b8f0f2fbc0e45
Mátyás Budavári <budavariam@gmail.com>
2025-02-23 22:16:43 +0100

    feat: :rocket: Add CV deployment

---
+++ gh-pages.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+FILE="budavariam.pdf"
+BRANCH="gh-pages"
+
+docker compose up || exit 1
+
+git branch -D "$BRANCH"
+git push -d origin "$BRANCH"
+git checkout --orphan "$BRANCH" || exit 2
+rm -rf ./.gitignore ./.vscode
+shopt -s extglob
+rm -rf !(budavariam.pdf)  2> /dev/null
+git reset
+git add "./$FILE"
+git commit -m "CV"
+git push origin "$BRANCH" --force
+git checkout main
+git reset --hard HEAD
